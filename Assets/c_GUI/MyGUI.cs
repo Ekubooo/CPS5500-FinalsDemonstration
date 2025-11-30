4f677c0f965c6178d557b74393a1bde4f93605c7
PepperShaker <martherssteve@gmail.com>
2025-11-30 14:02:29 +0800

    update Fluid2D

---
+++ Assets/c_GUI/MyGUI.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+using TMPro;
+
+public class MyGUI : MonoBehaviour
+{
+    [SerializeField]
+    TextMeshProUGUI myDisplay;
+
+    [SerializeField, Range(0.1f, 2f)]
+    float sampleDuration = 1f;
+
+    int _frames;
+    float _duration, _bestDuration = float.MaxValue, _worstDuration;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        float frameDuration = Time.unscaledDeltaTime;
+        _frames++;
+        _duration += frameDuration;
+        if (frameDuration < _bestDuration) {
+            _bestDuration = frameDuration;
+        }
+        if (frameDuration > _worstDuration) {
+            _worstDuration = frameDuration;
+        }
+        if (_duration >= sampleDuration)
+        {
+            myDisplay.SetText("FPS\n{0:0}",
+                _frames / _duration);
+            _frames = 0;
+            _duration = 0f;
+            _bestDuration = float.MaxValue;
+            _worstDuration = 0f;
+        }
+    }
+}
